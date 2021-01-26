7eaa537142222a52b51a63162c0fc81aa85f587c
FaimTonmoy <bhuiyanfaim@gmail.com>
2021-01-27 00:38:30 +0600

    Add files via upload

---
+++ svm.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import KFold
+
+df=pd.read_csv('lungcancer.csv')
+X= df.iloc[:,1:].values
+y=df.iloc[:,0]
+
+
+
+imputer= SimpleImputer(missing_values=np.nan, strategy='median')
+imputer=imputer.fit_transform(X)
+
+X= pd.DataFrame(imputer)
+
+conf_matrix_list_of_arrays = []
+acc_list_of_arrays=[]
+f1_list_of_arrays=[]
+kf=KFold(n_splits=5, random_state=3, shuffle=True)
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X.loc[train_index, :], X.loc[test_index, :]
+    y_train, y_test = y[train_index], y[test_index]
+    cls = svm.SVC(kernel="rbf")
+    cls.fit(X_train,y_train)
+    y_pred=cls.predict(X_test)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    acc= metrics.accuracy_score(y_test,y_pred)
+    f1=metrics.f1_score(y_test,y_pred, average='micro')
+    conf_matrix_list_of_arrays.append(conf_matrix)
+    acc_list_of_arrays.append(acc)
+    f1_list_of_arrays.append(f1)
+print("SVM")
+print("Confusion Matrix")
+print(conf_matrix_list_of_arrays)
+print("Accuracy")
+print(acc_list_of_arrays)
+print("F1 score")
+print(f1_list_of_arrays)
